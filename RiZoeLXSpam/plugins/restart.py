1dfd8383529bb74692c1230e3e243ee968ad6121
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2021-12-20 11:51:21 +0530

    Update restart.py

--- RiZoeLXSpam/plugins/restart.py
+++ RiZoeLXSpam/plugins/restart.py
@@ -1,20 +1,30 @@
-from .. import Riz, Riz2, Riz3, Riz4, Riz5, Riz6, Riz7, Riz8, Riz9, Riz10, SUDO_USERS
+from .. import Riz, Riz2, Riz3, Riz4, Riz5 , Riz6, Riz7, Riz8, Riz9, Riz10, Riz11, Riz12, Riz13, Riz14, Riz15, Riz16, Riz17, Riz18, Riz19, Riz20, SUDO_USERS
 from telethon import events
 import os
 import random
 import sys
 
 
-@Riz.on(events.NewMessage(pattern=".reboot"))
-@Riz2.on(events.NewMessage(pattern=".reboot"))
-@Riz3.on(events.NewMessage(pattern=".reboot"))
-@Riz4.on(events.NewMessage(pattern=".reboot"))
-@Riz5.on(events.NewMessage(pattern=".reboot"))
-@Riz6.on(events.NewMessage(pattern=".reboot"))
-@Riz7.on(events.NewMessage(pattern=".reboot"))
-@Riz8.on(events.NewMessage(pattern=".reboot"))
-@Riz9.on(events.NewMessage(pattern=".reboot"))
-@Riz10.on(events.NewMessage(pattern=".reboot"))
+@Riz.on(events.NewMessage(pattern=r"\.reboot"))
+@Riz2.on(events.NewMessage(pattern=r"\.reboot"))
+@Riz3.on(events.NewMessage(pattern=r"\.reboot"))
+@Riz4.on(events.NewMessage(pattern=r"\.reboot"))
+@Riz5.on(events.NewMessage(pattern=r"\.reboot"))
+@Riz6.on(events.NewMessage(pattern=r"\.reboot"))
+@Riz7.on(events.NewMessage(pattern=r"\.reboot"))
+@Riz8.on(events.NewMessage(pattern=r"\.reboot"))
+@Riz9.on(events.NewMessage(pattern=r"\.reboot"))
+@Riz10.on(events.NewMessage(pattern=r"\.reboot"))
+@Riz11.on(events.NewMessage(pattern=r"\.reboot"))
+@Riz12.on(events.NewMessage(pattern=r"\.reboot"))
+@Riz13.on(events.NewMessage(pattern=r"\.reboot"))
+@Riz14.on(events.NewMessage(pattern=r"\.reboot"))
+@Riz15.on(events.NewMessage(pattern=r"\.reboot"))
+@Riz16.on(events.NewMessage(pattern=r"\.reboot"))
+@Riz17.on(events.NewMessage(pattern=r"\.reboot"))
+@Riz18.on(events.NewMessage(pattern=r"\.reboot"))
+@Riz19.on(events.NewMessage(pattern=r"\.reboot"))
+@Riz20.on(events.NewMessage(pattern=r"\.reboot"))
 async def restart(e):
     if e.sender_id in SUDO_USERS:
         text = "**Rebooting Your MLO Spam Bots**.. Please Wait Until It Starts Again"
@@ -57,6 +67,46 @@ async def restart(e):
             pass
         try:
             await Riz10.disconnect()
+        except Exception:
+            pass
+        try:
+            await Riz11.disconnect()
+        except Exception:
+            pass
+        try:
+            await Riz12.disconnect()
+        except Exception:
+            pass
+        try:
+            await Riz13.disconnect()
+        except Exception:
+            pass
+        try:
+            await Riz14.disconnect()
+        except Exception:
+            pass
+        try:
+            await Riz15.disconnect()
+        except Exception:
+            pass
+        try:
+            await Riz16.disconnect()
+        except Exception:
+            pass
+        try:
+            await Riz17.disconnect()
+        except Exception:
+            pass
+        try:
+            await Riz18.disconnect()
+        except Exception:
+            pass
+        try:
+            await Riz19.disconnect()
+        except Exception:
+            pass
+        try:
+            await Riz20.disconnect()
         except Exception:
             pass
 
