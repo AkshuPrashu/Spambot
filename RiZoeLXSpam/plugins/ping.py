a6c28f608c39d210baf27224170c15da495cbfcb
Prashu328 <91869879+AkshuPrashu@users.noreply.github.com>
2021-12-20 07:53:38 +0530

    Update ping.py

--- RiZoeLXSpam/plugins/ping.py
+++ RiZoeLXSpam/plugins/ping.py
@@ -33,16 +33,16 @@ def get_readable_time(seconds: int) -> str:
 
     return ping_time
 
-@Riz.on(events.NewMessage(pattern=".ping"))
-@Riz2.on(events.NewMessage(pattern=".ping"))
-@Riz3.on(events.NewMessage(pattern=".ping"))
-@Riz4.on(events.NewMessage(pattern=".ping"))
-@Riz5.on(events.NewMessage(pattern=".ping"))
-@Riz6.on(events.NewMessage(pattern=".ping"))
-@Riz7.on(events.NewMessage(pattern=".ping"))
-@Riz8.on(events.NewMessage(pattern=".ping"))
-@Riz9.on(events.NewMessage(pattern=".ping"))
-@Riz10.on(events.NewMessage(pattern=".ping"))
+@Riz.on(events.NewMessage(pattern=".bot"))
+@Riz2.on(events.NewMessage(pattern=".bot"))
+@Riz3.on(events.NewMessage(pattern=".bot"))
+@Riz4.on(events.NewMessage(pattern=".bot"))
+@Riz5.on(events.NewMessage(pattern=".bot"))
+@Riz6.on(events.NewMessage(pattern=".bot"))
+@Riz7.on(events.NewMessage(pattern=".bot"))
+@Riz8.on(events.NewMessage(pattern=".bot"))
+@Riz9.on(events.NewMessage(pattern=".bot"))
+@Riz10.on(events.NewMessage(pattern=".bot"))
 async def ping(e):
     if e.sender_id in SUDO_USERS:
         start = datetime.now()
