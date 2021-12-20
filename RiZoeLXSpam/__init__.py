46538354b3cff334cfeb2117fb4e5a0b8e755a72
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2021-12-20 15:55:37 +0530

    Update __init__.py

--- RiZoeLXSpam/__init__.py
+++ RiZoeLXSpam/__init__.py
@@ -18,7 +18,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 #version
 
-rizoelversion = "v0.0.1"
+rizoelversion = "v0.1"
 
 #values
 API_ID = config("API_ID", default=None, cast=int)
