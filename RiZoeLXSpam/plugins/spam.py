500bf19314063d45de0aff82aa4b1f8842331c25
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-01-15 01:10:04 +0530

    Update spam.py

--- RiZoeLXSpam/plugins/spam.py
+++ RiZoeLXSpam/plugins/spam.py
@@ -20,6 +20,8 @@ from telethon import events
 from telethon import functions, types
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 from .. import Riz, Riz2, Riz3, Riz4, Riz5 , Riz6, Riz7, Riz8, Riz9, Riz10, Riz11, Riz12, Riz13, Riz14, Riz15, Riz16, Riz17, Riz18, Riz19, Riz20, SUDO_USERS
+from resources.data import GROUP, PORMS
+
 
 SMEX_USERS = []
 for x in SUDO_USERS:
@@ -76,3 +78,44 @@ async def spam(e):
             await asyncio.wait([e.respond(message) for i in range(counter)])
         else:
             await e.reply(usage, parse_mode=None, link_preview=None)
+
+@Riz.on(events.NewMessage(pattern=r"\.pspam"))
+@Riz2.on(events.NewMessage(pattern=r"\.pspam"))
+@Riz3.on(events.NewMessage(pattern=r"\.pspam"))
+@Riz4.on(events.NewMessage(pattern=r"\.pspam"))
+@Riz5.on(events.NewMessage(pattern=r"\.pspam"))
+@Riz6.on(events.NewMessage(pattern=r"\.pspam"))
+@Riz7.on(events.NewMessage(pattern=r"\.pspam"))
+@Riz8.on(events.NewMessage(pattern=r"\.pspam"))
+@Riz9.on(events.NewMessage(pattern=r"\.pspam"))
+@Riz10.on(events.NewMessage(pattern=r"\.pspam"))
+@Riz11.on(events.NewMessage(pattern=r"\.pspam"))
+@Riz12.on(events.NewMessage(pattern=r"\.pspam"))
+@Riz13.on(events.NewMessage(pattern=r"\.pspam"))
+@Riz14.on(events.NewMessage(pattern=r"\.pspam"))
+@Riz15.on(events.NewMessage(pattern=r"\.pspam"))
+@Riz16.on(events.NewMessage(pattern=r"\.pspam"))
+@Riz17.on(events.NewMessage(pattern=r"\.pspam"))
+@Riz18.on(events.NewMessage(pattern=r"\.pspam"))
+@Riz19.on(events.NewMessage(pattern=r"\.pspam"))
+@Riz20.on(events.NewMessage(pattern=r"\.pspam"))
+async def pspam(e):
+    if e.sender_id in SMEX_USERS:
+        if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
+            return await e.reply(usage, parse_mode=None, link_preview=None )
+        rizoel = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        if len(rizoel) == 1:
+            counter = int(rizoel[0])
+            if int(e.chat_id) in GROUP:
+                text = f"Sorry !! I can't spam here"
+                await e.reply(text, parse_mode=None, link_preview=None )
+            else:
+                 porrn = random.choice(PORMS)
+                 for _ in range(counter):
+                     async with e.client.action(e.chat_id, "document"):
+                         smex = await e.client.send_file(e.chat_id, porrn)
+                         await gifspam(e, smex) 
+                     await asyncio.sleep(0.4)
+        else:
+            usage = f"**MODULE NAME : PORN SPAM** \n\n command: `.pornspam <count>`"
+            await e.reply(usage, parse_mode=None, link_preview=None )
